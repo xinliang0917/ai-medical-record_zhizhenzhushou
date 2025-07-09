8d3672b0091160461d0ab572ed1d3ff69eb92e25
xinliang <3234196651@qq.com>
2025-07-09 11:17:17 +0800

    部署上线

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ai-medical-record/' // <-- 这里替换成你的 GitHub 仓库名称，例如 '/ai-medical-assistant-frontend/'
+    : '/',
+  outputDir: 'dist',
 })
